567dcc6e404ad19242a1f0c8ac20bde226b783c0
santospanda111 <santospanda111@gmail.com>
2021-05-20 09:08:08 +0530

    Added test case UC2 check BinaryTree size and addition of values

--- src/test/java/com/binarytree/BinaryTreeTest.java
+++ src/test/java/com/binarytree/BinaryTreeTest.java
@@ -18,4 +18,26 @@ public class BinaryTreeTest {
         binaryTree.add(70);
         Assertions.assertEquals(3, binaryTree.size());
     }
+
+    /**
+     * this test case will check the size and successful addition of values.
+     */
+    @Test
+    public void Elements_Added_To_Create_BinaryTree_Returns_Size13() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        binaryTree.add(22);
+        binaryTree.add(40);
+        binaryTree.add(60);
+        binaryTree.add(95);
+        binaryTree.add(11);
+        binaryTree.add(65);
+        binaryTree.add(3);
+        binaryTree.add(16);
+        binaryTree.add(63);
+        binaryTree.add(67);
+        Assertions.assertEquals(13, binaryTree.size());
+    }
 }
